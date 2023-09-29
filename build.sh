3a8fcbcaa94becb44851a468cb3e52d61c7573a9
lmartin <hello@leejohnmartin.co.uk>
2023-09-29 22:40:03 +0100

    [skip ci]: podman host script as helper

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -v -p ~/.local/share/godot/templates
-mv /root/.local/share/godot/templates/3.5.2.stable.mono ~/.local/share/godot/templates/3.5.2.stable.mono
-mkdir -v -p /build/build/web
-godot -v --export "HTML5" /build/build/web/index.html
