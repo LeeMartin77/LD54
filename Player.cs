e68f8ed4ca8d3d42ad735dcc5f06e8260d816397
lmartin <hello@leejohnmartin.co.uk>
2023-09-30 15:18:37 +0100

    added: Buttons (don't seem to work? But nice vis)

--- Player.cs
+++ Player.cs
@@ -5,8 +5,8 @@ public class Player : RigidBody2D
 {
   private Camera _camera;
 
-	[Export]
-	public float SpeedLimit = 500f;
+  [Export]
+  public float SpeedLimit = 500f;
   [Export]
   public float MaxExcessSpeedLimitSeconds = 5.0f;
 
@@ -28,161 +28,163 @@ public class Player : RigidBody2D
   [Export]
   public float RotationSpeed = 2f;
 
-	[Export]
-	public float DefaultFriction = 0.5f;
+  [Export]
+  public float DefaultFriction = 0.5f;
 
 
   private Sprite _closestPointMarker;
 
-	private Line2D _filament;
+  private Line2D _filament;
 
-	private Vector2 _closestFilamentPoint;
+  private Vector2 _closestFilamentPoint;
 
-	private bool _inEndZone = false;
-	private float _endzoneTime = 0.0f;
+  private bool _inEndZone = false;
+  private float _endzoneTime = 0.0f;
 
-	private float _timeInDangerZone = 0.0f;
+  private float _timeInDangerZone = 0.0f;
 
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
-	{
+  {
 
-		_camera = GetNode<Camera>("/root/World/Camera");
+    _camera = GetNode<Camera>("/root/World/Camera");
 
-		_filament = GetNode<Line2D>("/root/World/Filament");
-		_closestPointMarker = GetNode<Sprite>("/root/World/ClosestPoint");
+    _filament = GetNode<Line2D>("/root/World/Filament");
+    _closestPointMarker = GetNode<Sprite>("/root/World/ClosestPoint");
 
-		Friction = DefaultFriction;
+    Friction = DefaultFriction;
   }
 
-	public void SetEndZone(bool inZone)
-	{
-		_inEndZone = inZone;
-		_endzoneTime = 0.0f;
+  public void SetEndZone(bool inZone)
+  {
+    _inEndZone = inZone;
+    _endzoneTime = 0.0f;
   }
 
-	public float Speed {  get
-		{
-			return Math.Abs(LinearVelocity.x) + Math.Abs(LinearVelocity.y);
+  public float Speed
+  {
+    get
+    {
+      return Math.Abs(LinearVelocity.x) + Math.Abs(LinearVelocity.y);
 
-	} }
+    }
+  }
 
-	public bool Alive
-	{
-		get
-		{
-			return _timeInDangerZone < FilamentMaxDangerPeriodSeconds;
-		}
-	}
+  public bool Alive
+  {
+    get
+    {
+      return _timeInDangerZone < FilamentMaxDangerPeriodSeconds;
+    }
+  }
 
-	public bool Success
-	{
+  public bool Success
+  {
 
-	get
-	{
-	  return _endzoneTime > RequiredEndzoneTime;
-	}
+    get
+    {
+      return _endzoneTime > RequiredEndzoneTime;
+    }
   }
 
   public override void _PhysicsProcess(float delta)
   {
-		base._Process(delta);
+    base._Process(delta);
 
-		if (Success)
-			{
-				return;
-			}
+    if (Success)
+    {
+      return;
+    }
 
-		if (!Alive)
-			{
-			GD.Print(FilamentMaxDangerPeriodSeconds);
-				Friction = 0;
-				return;
-			}
-
-
-		_camera.Position = Position;
-		_camera.Rotation = Rotation;
-
-		var lastPoint = _filament.Points[0];
-		
-		Vector2 closestPoint = lastPoint; // bit of a cheaty hack
-		foreach (var vec in _filament.Points)
-		{
-			var loopPoint = Geometry.GetClosestPointToSegment2d(Position, lastPoint, vec);
-				lastPoint = vec;
-
-			if (Position.DistanceTo(loopPoint) < Position.DistanceTo(closestPoint))
-			{
-				closestPoint = loopPoint;
-			}
-		}
-		_closestFilamentPoint = closestPoint;
+    if (!Alive)
+    {
+      Friction = 0;
+      return;
+    }
 
 
-	  var distance = Position.DistanceTo(_closestFilamentPoint);
+    _camera.Position = Position;
+    _camera.Rotation = Rotation;
 
-	  if (distance < FilamentDangerStart)
-	  {
-			//// Friction 0 in danger zone 
-			Friction = 0;
-			_timeInDangerZone += delta;
-	  }
-	  else if (distance < FilamentRideStart)
-	  {
-			//otherwise falloff into danger zone
-			var distanceToDanger = distance - FilamentDangerStart;
-			// straight percentage for now but kinda want a curve
-			var fraction = distanceToDanger / (FilamentRideStart - FilamentDangerStart);
-			Friction = DefaultFriction * fraction;
-			_timeInDangerZone = 0;
-		}
-	  else
-	  {
-			// Friction is default
-			Friction = DefaultFriction;
-			_timeInDangerZone = 0;
-		}
+    var lastPoint = _filament.Points[0];
 
-		if (Input.IsActionPressed("up"))
-		{
-			// We only ever go "up"
-			Vector2 angledThrust = (new Vector2(0, -1) * (delta * Thrust)).Rotated(Rotation);
-			LinearVelocity += angledThrust;
-		}
-		if (Input.IsActionPressed("left"))
-		{
-			AngularVelocity += RotationSpeed * delta;
-		}
-		if (Input.IsActionPressed("right"))
-		{
-			AngularVelocity -= RotationSpeed * delta;
-		}
+    Vector2 closestPoint = lastPoint; // bit of a cheaty hack
+    foreach (var vec in _filament.Points)
+    {
+      var loopPoint = Geometry.GetClosestPointToSegment2d(Position, lastPoint, vec);
+      lastPoint = vec;
+
+      if (Position.DistanceTo(loopPoint) < Position.DistanceTo(closestPoint))
+      {
+        closestPoint = loopPoint;
+      }
+    }
+    _closestFilamentPoint = closestPoint;
+
+
+    var distance = Position.DistanceTo(_closestFilamentPoint);
+
+    if (distance < FilamentDangerStart)
+    {
+      //// Friction 0 in danger zone 
+      Friction = 0;
+      _timeInDangerZone += delta;
+    }
+    else if (distance < FilamentRideStart)
+    {
+      //otherwise falloff into danger zone
+      var distanceToDanger = distance - FilamentDangerStart;
+      // straight percentage for now but kinda want a curve
+      var fraction = distanceToDanger / (FilamentRideStart - FilamentDangerStart);
+      Friction = DefaultFriction * fraction;
+      _timeInDangerZone = 0;
+    }
+    else
+    {
+      // Friction is default
+      Friction = DefaultFriction;
+      _timeInDangerZone = 0;
+    }
+
+    if (Input.IsActionPressed("up"))
+    {
+      // We only ever go "up"
+      Vector2 angledThrust = (new Vector2(0, -1) * (delta * Thrust)).Rotated(Rotation);
+      LinearVelocity += angledThrust;
+    }
+    if (Input.IsActionPressed("left"))
+    {
+      AngularVelocity += RotationSpeed * delta;
+    }
+    if (Input.IsActionPressed("right"))
+    {
+      AngularVelocity -= RotationSpeed * delta;
+    }
   }
 
 
   public override void _Process(float delta)
   {
-		if (_closestFilamentPoint != null)
-			{
-				_closestPointMarker.Position = _closestFilamentPoint;
-		}
-		if (_inEndZone)
-		{
-				_endzoneTime += delta;
-		}
+    if (_closestFilamentPoint != null)
+    {
+      _closestPointMarker.Position = _closestFilamentPoint;
+    }
+    if (_inEndZone)
+    {
+      _endzoneTime += delta;
+    }
 
-		_camera.UpdateUI(new UIUpdate
-		{
-			PlayerAlive = Alive,
-			PlayerVictorious = Success,
-			Speed = Speed,
-			Proximity = Position.DistanceTo(_closestFilamentPoint),
-			Friction = Friction,
-			MaxFriction = DefaultFriction,
-			TimeTilDestruction = (FilamentMaxDangerPeriodSeconds, _timeInDangerZone)
-		});
+    _camera.UpdateUI(new UIUpdate
+    {
+      PlayerAlive = Alive,
+      PlayerVictorious = Success,
+      Speed = Speed,
+      Proximity = Position.DistanceTo(_closestFilamentPoint),
+      Friction = Friction,
+      MaxFriction = DefaultFriction,
+      TimeTilDestruction = (FilamentMaxDangerPeriodSeconds, _timeInDangerZone)
+    });
 
-		base._Process(delta);
+    base._Process(delta);
   }
 }
--- UI.cs
+++ UI.cs
@@ -11,11 +11,22 @@ public class UI : Control
   private RichTextLabel _frc;
   private RichTextLabel _alert;
 
+  private (TouchScreenButton, RichTextLabel) _thrust;
+  private (TouchScreenButton, RichTextLabel) _left;
+  private (TouchScreenButton, RichTextLabel) _right;
+  private (TouchScreenButton, RichTextLabel) _reset;
+
+
   private TextureRect[] _friction;
   private TextureRect[] _proximity;
 
+  private UIUpdate _latestUpdate = new UIUpdate { };
+
+  public Color OffColor = new Color(10, 10, 10);
+  public Color LitButton = new Color(255, 255, 0);
+
   public override void _Ready()
-	{
+  {
     _prx = GetNode<RichTextLabel>("PRX");
     _stat = GetNode<RichTextLabel>("STAT");
 
@@ -25,10 +36,17 @@ public class UI : Control
 
     _alert = GetNode<RichTextLabel>("ALERT");
 
+
+
     List<TextureRect> frictionLights = new List<TextureRect>();
     List<TextureRect> proximityLights = new List<TextureRect>();
 
-    foreach (var lbl in new string[] { "1", "2", "3", "4", "5"})
+    _thrust = (GetNode<TouchScreenButton>("Thrust"), GetNode<RichTextLabel>("Thrust/lbl"));
+    _left = (GetNode<TouchScreenButton>("Left"), GetNode<RichTextLabel>("Left/lbl"));
+    _right = (GetNode<TouchScreenButton>("Right"), GetNode<RichTextLabel>("Right/lbl"));
+    _reset = (GetNode<TouchScreenButton>("RESET"), GetNode<RichTextLabel>("RESET/lbl"));
+
+    foreach (var lbl in new string[] { "1", "2", "3", "4", "5" })
     {
       frictionLights.Add(GetNode<TextureRect>($"Friction/{lbl}"));
       proximityLights.Add(GetNode<TextureRect>($"Proximity/{lbl}"));
@@ -38,11 +56,60 @@ public class UI : Control
     _proximity = proximityLights.ToArray();
   }
 
-  public Color OffColor = new Color(10, 10, 10);
+  public override void _Process(float delta)
+  {
+    UpdateButtons();
+  }
 
+  private void UpdateButtons()
+  {
+    if (_latestUpdate.PlayerAlive)
+    {
+      _reset.Item1.Hide();
+      _reset.Item2.Hide();
+    }
+    else
+    {
+      _reset.Item1.Show();
+      _reset.Item2.Show();
+    }
+    if (_latestUpdate.PlayerAlive && Input.IsActionPressed("up"))
+    {
+      _thrust.Item1.Modulate = LitButton;
+      _thrust.Item2.Modulate = LitButton;
+    }
+    else
+    {
+      _thrust.Item1.Modulate = OffColor;
+      _thrust.Item2.Modulate = OffColor;
+    }
+    if (_latestUpdate.PlayerAlive && Input.IsActionPressed("left"))
+    {
+      _left.Item1.Modulate = LitButton;
+      _left.Item2.Modulate = LitButton;
+    }
+    else
+    {
+
+      _left.Item1.Modulate = OffColor;
+      _left.Item2.Modulate = OffColor;
+    }
+    if (_latestUpdate.PlayerAlive && Input.IsActionPressed("right"))
+    {
+      _right.Item1.Modulate = LitButton;
+      _right.Item2.Modulate = LitButton;
+    }
+    else
+    {
+
+      _right.Item1.Modulate = OffColor;
+      _right.Item2.Modulate = OffColor;
+    }
+  }
 
   public void UpdateData(UIUpdate update)
   {
+    _latestUpdate = update;
     if (update.PlayerAlive && !update.PlayerVictorious)
     {
       _alert.Hide();
