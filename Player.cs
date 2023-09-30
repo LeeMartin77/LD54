35f91b68cae0a690c82cea1846f52937adaed35e
lmartin <hello@leejohnmartin.co.uk>
2023-09-30 07:30:59 +0100

    fixed: Way simpler physics calc

--- Player.cs
+++ Player.cs
@@ -30,11 +30,13 @@ public class Player : RigidBody2D
 		{
 			// We only ever go "up"
 			// LinearVelocity is world tied
-			LinearVelocity = LinearVelocity + (new Vector2
+			float currentVelocity = Math.Abs(LinearVelocity.x) + Math.Abs(LinearVelocity.y);
+			currentVelocity = Math.Max(SpeedLimit, currentVelocity + (delta * Thrust));
+			LinearVelocity = new Vector2
 			{
 				x = 0,
-				y = -Math.Max(SpeedLimit, -LinearVelocity.y + (delta * Thrust))
-			}).Rotated(Rotation);
+				y = -currentVelocity
+			}.Rotated(Rotation);
 		}
 		if (Input.IsActionPressed("left"))
 		{
@@ -50,5 +52,6 @@ public class Player : RigidBody2D
   public override void _Process(float delta)
   {
 		base._Process(delta);
+
   }
 }
