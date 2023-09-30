06785cfb8e38797c2cc4695d847a93e58a1b9d5a
lmartin <hello@leejohnmartin.co.uk>
2023-09-30 07:52:35 +0100

    added: Some UI for speed/velocity

--- Player.cs
+++ Player.cs
@@ -14,11 +14,17 @@ public class Player : RigidBody2D
   [Export]
   public float RotationSpeed = 2f;
 
+	private RichTextLabel _speedLabel;
+  private RichTextLabel _rotationLabel;
+
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
 	{
 
 		_camera = GetNode<Camera2D>("Camera2D");
+
+		_speedLabel = GetNode<RichTextLabel>("UI/CurrentSpeed");
+		_rotationLabel = GetNode<RichTextLabel>("UI/CurrentRotation");
   }
 
 
@@ -30,6 +36,7 @@ public class Player : RigidBody2D
 		{
 			// We only ever go "up"
 			// LinearVelocity is world tied
+			// this is dumb maths unfuck it
 			float currentVelocity = Math.Abs(LinearVelocity.x) + Math.Abs(LinearVelocity.y);
 			currentVelocity = Math.Max(SpeedLimit, currentVelocity + (delta * Thrust));
 			LinearVelocity = new Vector2
@@ -52,6 +59,7 @@ public class Player : RigidBody2D
   public override void _Process(float delta)
   {
 		base._Process(delta);
-
+		_speedLabel.Text = $"{Math.Abs(LinearVelocity.x) + Math.Abs(LinearVelocity.y)} Speed";
+	_rotationLabel.Text = $"{RotationDegrees} Rotation";
   }
 }
