16e1e96faaa4489eed5ac66f4aebeee89563482c
lmartin <hello@leejohnmartin.co.uk>
2023-10-01 12:47:01 +0100

    visual: Replace particles with sprite

--- ClosestPoint.cs
+++ ClosestPoint.cs
@@ -5,7 +5,7 @@ public class ClosestPoint : Node2D
 {
   private Player _player;
   private Line2D _filament;
-  private CPUParticles2D _particles;
+  private AnimatedSprite _particles;
 
   int _maxParticles = 25;
 
@@ -13,49 +13,40 @@ public class ClosestPoint : Node2D
   {
     _player = GetNode<Player>("/root/World/Player");
     _filament = GetNode<Line2D>("/root/World/Filament");
-    _particles = GetNode<CPUParticles2D>("CPUParticles2D");
+    _particles = GetNode<AnimatedSprite>("AnimatedSprite");
   }
 
   public override void _Process(float delta)
   {
     var lastPoint = _filament.Points[0];
     Vector2 closestPoint = lastPoint; // bit of a cheaty hack
+    (Vector2, Vector2) segment = (lastPoint, lastPoint);
     foreach (var vec in _filament.Points)
     {
       var loopPoint = Geometry.GetClosestPointToSegment2d(_player.Position, lastPoint, vec);
-      lastPoint = vec;
 
       if (_player.Position.DistanceTo(loopPoint) < _player.Position.DistanceTo(closestPoint))
       {
         closestPoint = loopPoint;
+        segment = (lastPoint, vec);
       }
+      lastPoint = vec;
     }
+
     Position = closestPoint;
 
-    _particles.Direction = Position.DirectionTo(_player.Position);
-    //_particles.Position = Position;
+    Rotation = Position.AngleToPoint(_player.Position) + Mathf.Deg2Rad(90);
 
-    if (Position.DistanceTo(_player.Position) < _player.FilamentDangerStart)
+    if (Position.DistanceTo(_player.Position) < _player.FilamentRideStart)
     {
-      //_particles.Amount = _maxParticles;
-      _particles.Modulate = new Color(255, 255, 255, 1);
-    } 
-    else if (Position.DistanceTo(_player.Position) < _player.FilamentRideStart)
-    {
-      //var safeArea = Position.DistanceTo(_player.Position) - _player.FilamentDangerStart;
-      //var totalArea = _player.FilamentRideStart - _player.FilamentDangerStart;
-      //var rawPrtcls = _maxParticles - (_maxParticles * (safeArea / totalArea));
-      //var particles = Math.Max((int)Math.Round(rawPrtcls), 1);
-      //GD.Print(particles);
-      //_particles.Amount = particles;
-      var safeArea = Position.DistanceTo(_player.Position) - _player.FilamentDangerStart;
-      var totalArea = _player.FilamentRideStart - _player.FilamentDangerStart;
-      _particles.Modulate = new Color(255, 255, 255, 1 - (safeArea / totalArea));
+      _particles.Visible = true;
+      var closeness = 1 - (Position.DistanceTo(_player.Position) / _player.FilamentRideStart);
+      _particles.Modulate = new Color(255, 255, 255, closeness);
+      _particles.Scale = new Vector2(12, 24 * closeness);
     }
     else
     {
-      _particles.Modulate = new Color(255, 255, 255, 0);
-      //_particles.Amount = 1;
+      _particles.Visible = false;
     }
   }
 }
